a2f12cf18a615fbda08b0651e65465b8109adce1
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-04 10:00:49 +0900

    BitmapをBase64へエンコードするメソッドのコードを修正

--- app/src/main/java/com/caraquri/android_bookmanager/ImageUtil.java
+++ app/src/main/java/com/caraquri/android_bookmanager/ImageUtil.java
@@ -20,18 +20,11 @@ public class ImageUtil {
 
     // Bitmap画像をBase64にエンコード
     public static String encodeToBase64(Bitmap image) {
-        Bitmap immagex = image;
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        immagex.compress(Bitmap.CompressFormat.PNG, 0, byteArrayOutputStream); // 画像は高圧縮にする(画像が大きすぎると保存失敗率大のため)
+        image.compress(Bitmap.CompressFormat.PNG, 0, byteArrayOutputStream); // 画像は高圧縮にする(画像が大きすぎると保存失敗率大のため)
         byte[] bytes = byteArrayOutputStream.toByteArray();
         String imageEncoded = Base64.encodeToString(bytes, Base64.NO_WRAP);
         return imageEncoded;
-    }
-
-    // Base64をBitmap画像にデコード
-    public static Bitmap decodeToBase64(String input) {
-        byte[] decodedByte = Base64.decode(input, 0);
-        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
     }
 
     // ストレージが読み出し可能かどうかをチェック
